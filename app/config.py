b214bf7f08afd7b4d9f6ad05895052b7c239d3b8
matheusmv <matheus.matias.viana@hotmail.com>
2020-09-06 17:52:06 -0300

    configurando imports banco-de-dados run.py

---
+++ app/config.py
@@ -0,0 +1,8 @@
+DEBUG = True
+
+# ip = localhost, caso o acesso ao bd seja local
+uri_db = 'mysql+pymysql://usuario:senha@ip/nome-do-db'
+
+SQLALCHEMY_DATABASE_URI = uri_db
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- app/main/model/Sala.py
+++ app/main/model/Sala.py
@@ -1,4 +1,4 @@
 class Sala:
+
     def __init__(self, assentos):
         self.assentos = assentos
-        
